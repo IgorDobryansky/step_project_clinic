f41458e69c4a3ceabc970691ad5681a8abb248e8
IhorDobryanskiy <l48507100120@gmail.com>
2023-02-27 11:06:01 +0200

    some fix

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,18 @@ import "../index.html";
 import "../styles/index.scss";
 
 import Visit from "./prepare.js";
+import Login from "./Components/Login.js";
+import { getAllVisits, putVisit } from "./helpers/visitRequests.js";
 
-const visitsBoard = document.getElementById("visits");
+function app() {
+  const visitsBoard = document.getElementById("visits");
 
-visitsBoard.append(new Visit().render());
+  const loginStatus = document.getElementById("login-status");
+  loginStatus.append(new Login().render());
+
+  visitsBoard.append(new Visit().render());
+
+  //   getAllVisits().then((array) => console.log(array));
+}
+
+app();
