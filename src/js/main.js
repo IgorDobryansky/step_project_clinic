6479d03f15f3db68629c3cdb16691f00ecfb6f09
Elena Polyanskaya <helenpolyanskaya@gmail.com>
2023-02-28 17:30:22 +0100

    first commit

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,12 @@ import "../index.html";
 import "../styles/index.scss";
 
 import Visit from "./prepare.js";
+import Visit2 from "./prepare.js";
+
+import logo from "../img/daily-health-app (1).png"
+const image = document.createElement("img")
+//image.src = logo
+
 
 const visitsBoard = document.getElementById("visits");
 
-visitsBoard.append(new Visit().render());
