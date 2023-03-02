c3f622d00771fc51860f1f8359262942f2990292
Elena Polyanskaya <helenpolyanskaya@gmail.com>
2023-03-02 20:21:44 +0100

    Fix class visit, visitcardiol, visitdentist, visittherapist

---
+++ src/js/Components/Modal/ModalEditVisit.js
@@ -0,0 +1,24 @@
+//import ModalCreateVisit from "./ModalCreateVisit";
+
+
+////export default class ModalEditVisit extends ModalCreateVisit {
+//     constructor(){
+//         super();
+//         this._createVisitHeader = document.createElement("h2");
+//         this._createVisitHeader.innerText = "Відредагувати візит";
+//     }
+    
+//     this._doctorSelect.addEventListener("input", (event) => {
+//         const selectValue = this._doctorSelect.value;
+//         this._visitFormFields.innerHTML = "";
+//         if (selectValue === "Терапевт") {
+//             this._visitFormFields.append(
+//             this._visitPurposeFieldset,
+//             this._visitDescriptionFieldset,
+//             this._urgencySelectFieldset,
+//             this._fullNameInputFieldset,
+//             this._ageFieldset
+//         )}
+
+//     }  
+//}    
--- src/js/Components/Visit/VisitDentist.js
+++ src/js/Components/Visit/VisitDentist.js
@@ -3,12 +3,32 @@ import Visit from "./Visit.js";
 export default class VisitDentist extends Visit {
   constructor(responseObject) {
     super(responseObject);
-    this.lastVisitDate = document.createElement("p");
-    this.lastVisitDate.innerText = responseObject.lastVisitDate;
-    this._hidenFields.append(this.lastVisitDate);
+    this.lastVisit = responseObject.lastVisitDate;
+    //this.lastVisitDate.innerText = responseObject.lastVisitDate;
+    //this._hidenFields.append(this.lastVisitDate);
+  }
+
+  get lastVisit(){
+    return this._lastVisit
+  }
+
+  set lastVisit(value) {
+    if (!this._elLastVisit) {
+      this._elLastVisit = document.createElement('p');
+    }
+    this._elLastVisit.innerText = `Дата останнього відвідування: ${value};`;
+    this._lastVisit = value;
   }
 
   render(){
-    this._visitParentlement.append(this._visitDiv);
+    this.renderVisitBase();
+    this.visitAdd.append(this._elLastVisit); 
+    this.visitAdd.append(this.buttonHide);
+    this.visitAdd.append(this.buttonEdit); 
+    // this.visitAdd.append(this.buttonStatus); 
+    //this._visitParentlement.append(this._visitDiv);
   }
 }
+
+// let dobryanskyi = new VisitDentist("Добрянский Ігор Батьковіч", "Стоматолог", "10.09.2022");
+// dobryanskyi.render() 
